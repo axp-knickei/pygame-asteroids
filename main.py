c655f56b51d9f5c95a345f393e2253e21265197f
Alex Prima <alexprima@gmail.com>
2025-12-02 17:28:50 +0700

    Add basic pygame ini and game loop

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+def main():
+    print("Starting Asteroids with pygame version:", pygame.version.ver)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    while True:                    # Correction from Boots for infinite loop 
+        log_state()                # Call log_state(). No arguments needed
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")       # black without quotes is treated as variable.
+        pygame.display.flip()      # Use pygame's `display.flip()` method to refresh the screen.
+
+if __name__ == "__main__":
+    main()
